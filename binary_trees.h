b7dceaae1590e6e69a0408f37087ba9b6233bb5c
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-12-02 00:02:03 -0800

    file

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -30,8 +29,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* binary_tree_print.c */
-void binary_tree_print(const binary_tree_t *);
 
 /* tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -53,6 +50,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+/* binary_tree_print.c */
+void binary_tree_print(const binary_tree_t *);
 
 /* Advanced tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
